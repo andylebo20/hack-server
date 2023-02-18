ab5a045f90b076a3c9305b6610b7bdf84cba8210
andylebo20 <62567315+andylebo20@users.noreply.github.com>
2023-02-18 09:59:59 -0800

    grouped

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,9 +3,10 @@ import { UserController } from "src/controllers/UserController";
 
 const router = expressRouter();
 
-router.post("/book", UserController.bookProperty);
 router.get("/properties", UserController.getProperties);
 router.get("/property/:id", UserController.getPropertyById);
+
+router.post("/book", UserController.bookProperty);
 router.post("/property", UserController.createNewProperty);
 
 export default router;
